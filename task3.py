08266013524270968cb4c79f8d7f28bf654436de
Affsyan <Affsyan@gmail.com>
2021-03-03 21:17:37 +0300

    lesson2

--- task3.py
+++ task3.py
@@ -3,4 +3,13 @@
 Например, если введено число 3486, надо вывести 6843.
 """
 
-print(''.join(reversed(input("Введите натуральное число: "))).lstrip('0'))
+number = int(input("Введите натуральное число: "))
+number_2 = 0
+
+while number > 0:
+    digit = number % 10
+    number = number // 10
+    number_2 = number_2 * 10
+    number_2 = number_2 + digit
+
+print(f"Обратное число будет: {number_2} ")
