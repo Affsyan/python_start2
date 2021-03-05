b1568543d2b031a0b0b7a461b97748d2035ff415
Affsyan <Affsyan@gmail.com>
2021-03-05 19:40:33 +0300

    lesson3

---
+++ task4.py
@@ -0,0 +1,31 @@
+"""
+Определить, какое число в массиве встречается чаще всего.
+"""
+
+import random
+
+SIZE = 20
+MIN_ITEM = 0
+MAX_ITEM = 100
+
+numbers = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+
+print(numbers)
+element_index = 0
+count = 0
+count_max = 0
+element_max = 0
+count_while = 0
+
+while count_while != SIZE:
+    for i in range(len(numbers)):
+        if numbers[element_index] == numbers[i]:
+            count += 1
+        if count_max < count:
+            count_max = count
+            element_max = numbers[element_index]
+    count = 0
+    element_index += 1
+    count_while += 1
+
+print(f"Число {element_max} встречается в массиве {count_max} раз(а).")
